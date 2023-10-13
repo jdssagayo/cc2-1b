864bcf99a25f2250de04c060d2913fac0f7b9586
jdssagayo <145992021+jdssagayo@users.noreply.github.com>
2023-10-13 19:39:12 +0800

    Delete summation.py

--- summation.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter a number: "))
-
-sum_of_numbers = 0
-
-for i in range(1, n + 1):
-    sum_of_numbers += i
-
-print("The over all sum of numbers from 1 to", n, "is:", sum_of_numbers)
