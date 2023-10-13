740983a870419fc523e09c5d26472ecc60811c8e
jdssagayo <145992021+jdssagayo@users.noreply.github.com>
2023-10-13 19:15:15 +0800

    Create right_triangle.py

---
+++ right_triangle.py
@@ -0,0 +1,7 @@
+n = 5
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()  # Add a newline for vertical spacing
+    print()  # Add an additional newline for more vertical spacing
